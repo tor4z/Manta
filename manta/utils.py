3532bffcb769bee41a38030d2741a7dfebec5ed9
tor4z <vwenjie@hotmail.com>
2018-04-09 22:15:26 +0800

    fix indent error

--- manta/utils.py
+++ manta/utils.py
@@ -4,13 +4,13 @@ def run_dir(func_name=""):
     """
         This function *MUST* in the __init__.py
     """
-	for loader, mod_name, is_pkg in pkgutil.iter_modules([os.path.dirname(__file__)]):
-		try:
-			mod = loader.find_module(mod_name).load_module(mod_name)
-			for item in dir(mod):
+    for loader, mod_name, is_pkg in pkgutil.iter_modules([os.path.dirname(__file__)]):
+        try:
+            mod = loader.find_module(mod_name).load_module(mod_name)
+            for item in dir(mod):
                 func = getattr(mod, item)
                 if callable(func):
                     if not func_name or func.__name__ is func_name:
                         func()
-		except AttributeError:
-			pass
+        except AttributeError:
+            pass
