d01ae0437b5dccdb6e0fab0b7e07fa5a1d11d456
tor4z <vwenjie@hotmail.com>
2018-04-09 23:45:20 +0800

    fix test method

--- test/test_task.py
+++ test/test_task.py
@@ -13,25 +13,25 @@ class TestTask(unittest.TestCase):
         self.assertEqual(task.do(), 1)
 
     def test_task_lt(self):
-        task1 = task = Task(func, 0, 0, 1, callback=func_callback)
-        task2 = task = Task(func, 1, 0, 1, callback=func_callback)
+        task1 = Task(func, 0, 0, 1, callback=func_callback)
+        task2 = Task(func, 1, 0, 1, callback=func_callback)
         self.assertGreater(task2, task1)
 
     def test_task_normal(self):
-        task = task = Task(func, 0, 0, 1, callback=func_callback)
+        task = Task(func, 0, 0, 1, callback=func_callback)
         self.assertTrue(task.is_normal)
 
     def test_task_period(self):
-        task = task = Task(func, 0, 1, 1, callback=func_callback)
+        task = Task(func, 0, 1, 1, callback=func_callback)
         self.assertTrue(task.is_period)
 
     def test_task_timeout(self):
-        task = task = Task(func, 1, 0, 1, callback=func_callback)
+        task = Task(func, 1, 0, 1, callback=func_callback)
         self.assertTrue(task.is_timeout)
 
     def test_task_update_time(self):
         import time
-        task = task = Task(func, 0, 1, 1, callback=func_callback)
+        task = Task(func, 0, 1, 1, callback=func_callback)
         task.update_time()
         time.sleep(1)
         self.assertTrue(task.runable)
