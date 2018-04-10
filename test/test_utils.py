517cb45a2bf28f0327eb4ba6f6d8e07fa799ce9a
tor4z <vwenjie@hotmail.com>
2018-04-10 17:03:56 +0800

    Update utils test, add test log

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,5 +1,5 @@
 import unittest, os
-from manta.utils import run_dir
+from manta.utils import run_dir, Log
 
 var = 1
 def test_rundir():
@@ -10,4 +10,17 @@ class TestUtils(unittest.TestCase):
     def test_run_dir(self):
         run_dir(os.path.dirname(__file__), "test_rundir")
         self.assertNotEqual(var, 1)
+
+class TestLog(unittest.TestCase):
+    def test_log(self):
+        print()
+        Log.set_level(Log.NOTSET)
+        Log.set_name("test")
+
+        Log.info("info msg")
+        Log.debug("debug msg")
+        Log.warning("warning msg")
+        Log.error("error msg")
+        Log.fatal("fatal msg")
+        Log.critical("critical msg")
 
